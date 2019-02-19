4ebac8a116c17315c2d5a578bf8a1f6696771ce1
Colin Fendrick <colin.fendrick@gmail.com>
2019-02-19 13:40:21 -0500

    Create text

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTrail } from 'react-spring'
 
 import { Panel, MainText } from '.'
@@ -7,20 +7,22 @@ const colors = ["#E8CCAD", "#DCA090", "#D0757E", "#C55C87", "#B94599", "#A931AD"
 const config = { mass: 5, tension: 2000, friction: 200 }
 
 export default ({ children }) => {
+  const [toggle, set] = useState(true)
+
   const trail = useTrail(8, {
     config,
-    opacity: 1,
-    flex: 1,
+    opacity: toggle ? 1 : 0,
+    flex: toggle ? 1 : 0,
     from: { opacity: 0, flex: 0.1 }
   })
 
   return (
-    <div className="background">
+    <div className="background" onClick={() => set(state => !state)}>
       {trail.map((props, index) =>
         (index <= colors.length - 1) ?
         <Panel key={index} backgroundColor={colors[index]} style={props} />
         :
-          <MainText key={index} style={props} />
+        <MainText key={index} style={props} />
       )}
     </div>
   )
