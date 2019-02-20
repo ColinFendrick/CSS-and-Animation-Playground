bda7d0b4807a4c0a114435e2f40553577464a3d5
Colin Fendrick <colin.fendrick@gmail.com>
2019-02-20 08:15:17 -0500

    random color scheme

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useTrail } from 'react-spring'
-import randomColor from 'randomcolor';
+import ColorScheme from 'color-scheme'
 
 import { Panel, MainText } from '.'
 const config = { mass: 5, tension: 2000, friction: 200 }
@@ -8,22 +8,25 @@ const config = { mass: 5, tension: 2000, friction: 200 }
 export default () => {
   const [counter, setCounter] = useState(0)
 
-  const trail = useTrail(8, {
+  const genRanInt = max => Math.floor(Math.random() * Math.floor(max))
+
+  const trail = useTrail(9, {
     config,
     opacity: 1,
     flex: counter % 2 === 0 ? 1 : 0.3,
     from: { opacity: 0, flex: 0 }
   })
 
-  const colors = randomColor({ count: 7 })
+  const scheme = new ColorScheme();
+  const colors = scheme.from_hue(genRanInt(500)).scheme('contrast').variation('soft').colors()
 
   return (
     <div className="background" onClick={() => {
       setCounter(() => setCounter(counter + 1))
     }}>
       {trail.map((props, index) =>
-        (index <= colors.length - 1) ?
-        <Panel key={index}  style={{...props, backgroundColor: colors[index]}} />
+        (index < 8) ?
+        <Panel key={index}  style={{...props, backgroundColor: `#${colors[index]}`}} />
         :
         <MainText key={index} style={props} />
       )}
--- src/components/MainText.js
+++ src/components/MainText.js
@@ -3,5 +3,5 @@ import { animated } from 'react-spring'
 
 export default ({ style }) =>
   <animated.div className="main-wrapper" style={style}>
-    <div className="main-text">I love you <br /> click me and I change color</div>
+    <div className="main-text">I love you <br/> click me and I change color</div>
   </animated.div>
