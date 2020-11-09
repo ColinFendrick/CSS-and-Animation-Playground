ee59ee6587261618ebe5b4b02e9cbe4d7b85772c
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-08 17:30:40 -0800

    Remove backend for running on heroku

--- src/components/MyTooltip.js
+++ src/components/MyTooltip.js
@@ -1,43 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Tooltip, Spinner } from 'reactstrap';
-import axios from 'axios';
-
-const http = axios.create({
-	baseURL: 'http://localhost:3030',
-	headers: {
-		'Content-type': 'application/json'
-	}
-});
+import React, { useState } from 'react';
+import { Tooltip } from 'reactstrap';
 
 export default ({ target, country }) => {
 	const [tooltipOpen, setTooltipOpen] = useState(false);
-	const [loading, setLoading] = useState(false);
-	const [preview, setPreview] = useState(null);
 	const toggle = () => setTooltipOpen(!tooltipOpen);
-
-	useEffect(() => {
-		const source = axios.CancelToken.source();
-		(async () => {
-
-			if (!preview && tooltipOpen) {
-				setLoading(true);
-
-				try {
-					const previewData = await http.get(`http://localhost:3030/${country}`, {
-						cancelToken: source.token
-					});
-					setPreview(previewData.data);
-					setLoading(false);
-				} catch (e) {
-					setLoading(false);
-				}
-			}
-		})();
-
-		return () => {
-			source.cancel('Aborting request: user scrolled out');
-		};
-	}, [preview, tooltipOpen]);
 
 	return (
 		<Tooltip
@@ -49,11 +15,6 @@ export default ({ target, country }) => {
 		>
 			{country}:
 			<br />
-			<br />
-
-			{loading ? <Spinner color='primary' /> :
-				<img src={preview} alt='Wikipedia preview' className='website-preview' />
-			}
 		</Tooltip>
 	);
 };
